326a64a294e2a98fdcd5a1669896f07693508ea9
wooyeol <dnduf158@gmail.com>
2024-05-15 23:43:56 +0900

    add: register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:social_media_app/pages/login_page.dart';
+import 'package:social_media_app/auth/login_or_register.dart';
 import 'package:social_media_app/theme/dark_mode.dart';
 import 'package:social_media_app/theme/light_mode.dart';
 
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: const LoginOrRegister(),
       theme: lightMode,
       darkTheme: dartMode,
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:social_media_app/components/my_button.dart';
 import 'package:social_media_app/components/my_textfield.dart';
 
-class LoginPage extends StatelessWidget {
+class RegisterPage extends StatelessWidget {
   final void Function()? onTap;
 
-  LoginPage({
+  RegisterPage({
     super.key,
     required this.onTap,
   });
 
   // text controllers
+  final TextEditingController usernameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPwController = TextEditingController();
 
-  // login method
-  void login() {}
+  // register method
+  void register() {}
 
   @override
   Widget build(BuildContext context) {
@@ -44,6 +46,15 @@ class LoginPage extends StatelessWidget {
 
               const SizedBox(height: 50),
 
+              // username textfield
+              MyTextField(
+                hintText: "Username",
+                obscureText: false,
+                controller: usernameController,
+              ),
+
+              const SizedBox(height: 10),
+
               // email textfield
               MyTextField(
                 hintText: "Email",
@@ -62,24 +73,19 @@ class LoginPage extends StatelessWidget {
 
               const SizedBox(height: 10),
 
-              // forgot password
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary),
-                  ),
-                ],
+              // confirm password textfield
+              MyTextField(
+                hintText: "Confirm Password",
+                obscureText: true,
+                controller: confirmPwController,
               ),
 
               const SizedBox(height: 25),
 
-              // sign in button
+              // register in button
               MyButton(
-                text: "Login",
-                onTap: login,
+                text: "Register",
+                onTap: register,
               ),
 
               const SizedBox(height: 25),
@@ -89,14 +95,14 @@ class LoginPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text(
-                    "Don't have an account?",
+                    "Already have an account?",
                     style: TextStyle(
                         color: Theme.of(context).colorScheme.inversePrimary),
                   ),
                   GestureDetector(
                     onTap: onTap,
                     child: const Text(
-                      " Register Here",
+                      " Login Here",
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                       ),
